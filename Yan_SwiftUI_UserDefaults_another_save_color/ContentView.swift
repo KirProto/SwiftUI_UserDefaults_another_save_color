ef3bbe6de65fc0b6ab7d977ce9bcfadb9ec38d3d
Kir Proto Q <kir.proto@ya.ru>
2022-02-18 12:46:43 +0500

    Initial Commit

---
+++ Yan_SwiftUI_UserDefaults_another_save_color/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Yan_SwiftUI_UserDefaults_another_save_color
+//
+//  Created by KIR Q on 2022.02.18.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Yan_SwiftUI_UserDefaults_another_save_color/Yan_SwiftUI_UserDefaults_another_save_colorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Yan_SwiftUI_UserDefaults_another_save_colorApp.swift
+//  Yan_SwiftUI_UserDefaults_another_save_color
+//
+//  Created by KIR Q on 2022.02.18.
+//
+
+import SwiftUI
+
+@main
+struct Yan_SwiftUI_UserDefaults_another_save_colorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
